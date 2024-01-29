4c70f303db4a4a69a6b271f8a2272dd5186ac39b
Tom ツ <50146042+realt0w@users.noreply.github.com>
2024-01-30 00:13:44 +0100

    Pop-up

--- web_script.js
+++ web_script.js
@@ -1659,3 +1659,18 @@ refresh8button.addEventListener("click", function () {
 });
 
 init();
+
+
+// Afficher la pop-up lors du chargement de la page
+window.onload = function() {
+    var modal = document.getElementById("pop-up");
+    modal.style.display = "block";
+  };
+  
+  // Fermer la pop-up lors du clic sur le bouton de fermeture
+  var closeBtn = document.getElementsByClassName("close")[0];
+  closeBtn.onclick = function() {
+    var modal = document.getElementById("pop-up");
+    modal.style.display = "none";
+  };
+  
